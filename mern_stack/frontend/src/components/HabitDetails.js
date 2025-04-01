af328d4d278add1b9e58a5dfdc88ba44aef5bc39
Nico <rossi2nick@gmail.com>
2025-04-01 11:43:12 -0400

    added delete habit function

--- mern_stack/frontend/src/components/HabitDetails.js
+++ mern_stack/frontend/src/components/HabitDetails.js
@@ -1,9 +1,24 @@
-const HabitDetails = ({ habit }) => {
+import { useState } from 'react'
+
+const HabitDetails = ({ habit }) => { // add onDelete to chagne UI on deletion
+
+    const handleDelete = async () => {
+        const response = await fetch(`/api/habits/${habit._id}`, {
+            method: 'DELETE'
+        })
+
+        if (response.ok) {
+            //onDelete(habit._id) // Update UI after deletion
+        }
+    }
+
     return (
         <div className="habit-details">
             <h4>{habit.title}</h4>
             <p><strong>Description: </strong>{habit.description}</p>
             <p>{habit.createdAt}</p>
+            <button>mark as complete</button>
+            <button onClick={handleDelete}>delete habit</button>
         </div>
     )
 }
