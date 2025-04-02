3b1dc93d89e56cf3def0a872f86c4f9c728b556a
Joey Tran <joeytran71@gmail.com>
2025-04-02 19:36:27 -0400

    add more attributes to schema

--- mern_stack/backend/models/habitModel.js
+++ mern_stack/backend/models/habitModel.js
@@ -9,7 +9,35 @@ const habitSchema = new Schema({
     },
     description: {
         type: String
+    },
+    completions: [{
+        date: {
+            type: Date,
+            default: Date.now
+        },
+        completed: {
+            type: Boolean,
+            default: false
+        }
+    }],
+    currentStreak: {
+        type: Number,
+        default: 0
+    },
+    longestStreak: {
+        type: Number,
+        default: 0
+    },
+    frequency: {
+        type: String,
+        enum: ['daily', 'weekly', 'monthly'],
+        default: 'daily'
+    },
+    goal: {
+        type: Number,  // Target completions per period
+        default: 1
     }
+
 }, { timestamps: true })
 
 module.exports = mongoose.model('Habit', habitSchema)
