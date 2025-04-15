b8787411c8aca49971397d5d9ad483b30cd429d6
Nico <rossi2nick@gmail.com>
2025-04-15 13:33:50 -0400

    User Auth Backend

--- mern_stack/backend/server.js
+++ mern_stack/backend/server.js
@@ -3,8 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const habitRoutes = require('./routes/habits')
-
-
+const userRoutes = require('./routes/user')
 // express app
 const app = express()
 
@@ -18,13 +17,14 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/habits', habitRoutes)
+app.use('/api/user', userRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         // listen for requests
         app.listen(process.env.PORT, () => {
-            console.log('connected to db & listening on port', process.env.PORT)
+            console.log('Connected to database', process.env.PORT)
         })
     })
     .catch((error) => {
