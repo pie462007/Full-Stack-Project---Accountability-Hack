abf0a73a5ab1249526c990d4b9571dff76602600
harjot <harjotsingh201502@gmail.com>
2025-04-18 00:53:30 -0400

    spacing on navbar

--- mern_stack/frontend/src/components/Navbar.js
+++ mern_stack/frontend/src/components/Navbar.js
@@ -1,55 +1,51 @@
-import { Link } from 'react-router-dom' //npm install react-router-dom
-// import Friends from '../pages/Friends';
-// import Login from '../pages/Login';
-// import Home from '../pages/Home';
+import { Link } from 'react-router-dom'
 import '../styles/Navbar.css'
 import { useLogout } from '../hooks/useLogout'
 import { useAuthContext } from '../hooks/useAuthContext'
 
 const Navbar = () => {
-    const {logout} = useLogout()
-    const {user} = useAuthContext()
+  const { logout } = useLogout()
+  const { user } = useAuthContext()
 
-    const handleClick = () => {
-        logout()
-    }
+  const handleClick = () => {
+    logout()
+  }
 
-    return (
-        <header>
-            <nav className="container">
-                <div className = "nav-title">
-                    <h1>Accountability Hack</h1>
-                </div>
-                <div className = "header-div"> 
-                    <Link to="/">
-                        <h3>home</h3>
-                    </Link>
-                    <Link to="/friends">
-                        <h3>friends</h3>
-                    </Link>
-                    
-                    {!user && (
-                        <div>
-                            <Link to="/login">
-                                <h3>login</h3>
-                            </Link>
-                            <Link to="/signup">
-                                <h3>signup</h3>
-                             </Link>
-                        </div>
-                    )}
-                    
-                    {user && (
-                        <div>
-                        <span>{user.email}</span>
-                        <button onClick={handleClick}>Log out</button>
-                    </div>
-                    )}
-                </div>
-             
-            </nav>
-        </header>
-    )
+  return (
+    <header>
+      <nav className="container">
+        <div className="nav-title">
+          <h1>Accountability Hack</h1>
+        </div>
+        <div className="header-div"> {/* 👈 changed back to retain green border */}
+          <Link to="/">
+            <h3>home</h3>
+          </Link>
+          <Link to="/friends">
+            <h3>friends</h3>
+          </Link>
+
+          {!user && (
+            <>
+              <Link to="/login">
+                <h3>login</h3>
+              </Link>
+              <Link to="/signup">
+                <h3>signup</h3>
+              </Link>
+            </>
+          )}
+
+          {user && (
+            <div className="auth-links">
+              <span>{user.email}</span>
+              <button onClick={handleClick}>Log out</button>
+            </div>
+          )}
+        </div>
+      </nav>
+    </header>
+  )
 }
 
-export default Navbar;
+export default Navbar
